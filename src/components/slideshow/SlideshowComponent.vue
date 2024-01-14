0505bc8c72ec4f5718b681c22222993f5e4cefca
GTG4K <madtitan2002@gmail.com>
2024-01-14 23:55:53 +0400

    feat: automatic slide transition

--- src/components/slideshow/SlideshowComponent.vue
+++ src/components/slideshow/SlideshowComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {computed, ref} from 'vue';
+import {computed, onMounted, onUnmounted, ref} from 'vue';
 import SlideshowItemComponent from "./SlideshowItemComponent.vue";
 const slideshowContainerRef = ref<HTMLElement|null>(null)
 
@@ -19,13 +19,21 @@ const previousPreviousImage = computed(() => {
   return previousPreviousIndex >= 0 ? previousPreviousIndex : props.shows.length - 1;
 });
 const slideshowImageWidth = ref<number>(50) // in percentage
-
-
-
 const computedImageWidth = computed(()=>{
  if(slideshowContainerRef.value) return slideshowContainerRef.value.offsetWidth * slideshowImageWidth.value / 100
   return 0
 })
+
+const slideInterval = ref<any>(null);
+
+function startSlideShow() {
+  slideInterval.value = setInterval(viewNextShow, 7000);
+}
+
+function resetAndStartSlideshow() {
+  clearInterval(slideInterval.value);
+  startSlideShow();
+}
 
 const transitionInProgress = ref<boolean>(false)
 const animateSlideshowTranslateX = ref<number>(0)
@@ -42,9 +50,9 @@ function viewPreviousShow(){
       currentShow.value = currentShow.value - 1 === -1 ? props.shows.length - 1 : currentShow.value - 1
       animateSlideshowTranslateX.value = 0
     }, 550)
+    resetAndStartSlideshow()
   }
 }
-
 function viewNextShow(){
   if(!transitionInProgress.value) {
     transitionInProgress.value = true
@@ -54,8 +62,17 @@ function viewNextShow(){
       currentShow.value = currentShow.value + 1 < props.shows.length ? currentShow.value + 1 : 0
       animateSlideshowTranslateX.value = 0
     }, 550)
+    resetAndStartSlideshow()
   }
 }
+
+onMounted(() => {
+  startSlideShow();
+});
+
+onUnmounted(() => {
+  clearInterval(slideInterval.value);
+});
 </script>
 
 <template>
@@ -64,13 +81,17 @@ function viewNextShow(){
       <!-- Previous Previous Image -->
       <SlideshowItemComponent :show="props.shows[previousPreviousImage]" :slideshow-image-width="slideshowImageWidth" class="brightness-50" />
       <!-- Previous Image -->
-      <SlideshowItemComponent @click="viewPreviousShow" :show="props.shows[previousShow]" :slideshow-image-width="slideshowImageWidth" class="hover:transition-all hover:brightness-100"  :class="{'brightness-100': animateSlideshowTranslateX < 0, 'brightness-50': animateSlideshowTranslateX >= 0}"/>
+      <SlideshowItemComponent @click="viewPreviousShow" :show="props.shows[previousShow]" :slideshow-image-width="slideshowImageWidth" :class="{'brightness-100': animateSlideshowTranslateX < 0, 'brightness-50': animateSlideshowTranslateX >= 0}"/>
       <!-- Main Image -->
       <SlideshowItemComponent :show="props.shows[currentShow]" :slideshow-image-width="slideshowImageWidth" :class="{'brightness-50 transition': animateSlideshowTranslateX !== 0, 'brightness-100':animateSlideshowTranslateX === 0}"/>
       <!-- Next Image -->
-      <SlideshowItemComponent @click="viewNextShow" :show="props.shows[nextShow]" :slideshow-image-width="slideshowImageWidth" class="hover:transition-all hover:brightness-100" :class="{'brightness-100': animateSlideshowTranslateX > 0, 'brightness-50': animateSlideshowTranslateX <= 0 }"/>
+      <SlideshowItemComponent @click="viewNextShow" :show="props.shows[nextShow]" :slideshow-image-width="slideshowImageWidth" :class="{'brightness-100': animateSlideshowTranslateX > 0, 'brightness-50': animateSlideshowTranslateX <= 0 }"/>
       <!-- Next Next Image -->
       <SlideshowItemComponent :show="props.shows[nextNextShow]" :slideshow-image-width="slideshowImageWidth" class="brightness-50"/>
+    </div>
+    <div class="flex justify-between px-6 w-full absolute top-1/2 -translate-y-1/2">
+      <div @click="viewPreviousShow" class="transition-all cursor-pointer bg-gradient-to-br from-indigo-500/75 to-indigo-700/75 hover:scale-110 text-white font-bold py-6 px-5 rounded shadow-md select-none"> &lt </div>
+      <div @click="viewNextShow" class="transition-all cursor-pointer bg-gradient-to-br from-indigo-500/75 to-indigo-700/75 hover:scale-110 text-white font-bold py-6 px-5 rounded shadow-md select-none"> > </div>
     </div>
   </div>
 </template>
