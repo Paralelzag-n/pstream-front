3d916a0d5d62f522f7b2933fd46dd65f5ffaf165
Aleksandre Ochigava <aleksandre.cohigava@gmail.com>
2024-01-26 22:30:45 +0400

    feat: added validator for route change

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script lang="ts" setup>
 import TheHeader from "./components/layout/TheHeader.vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
 </script>
 
 <template>
   <main class="bg-dark-blue min-h-[100%]">
-    <TheHeader />
+    <TheHeader v-if="!route.name.startsWith('sign')" />
     <RouterView />
   </main>
 </template>
