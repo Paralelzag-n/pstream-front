d60190eb1fe5ba6b320db42eebf8beffff18adb3
Aleksandre Ochigava <aleksandre.cohigava@gmail.com>
2024-01-26 22:26:03 +0400

    feat: created home routes and added / route redirect

--- src/router/authentication.route.ts
+++ src/router/authentication.route.ts
@@ -1,9 +1,7 @@
-import RegisterViewVue from "../views/RegisterView.vue";
 const registerView = () => import("../views/RegisterView.vue");
 const signInView = () => import("../views/SignInView.vue");
 
 const AuthenticationRoutes = [
-  { path: "/", redirect: "/sign-in" },
   { path: "/sign-in", name: "sign-in", component: signInView },
   { path: "/sign-up", name: "sign-up", component: registerView },
 ];
